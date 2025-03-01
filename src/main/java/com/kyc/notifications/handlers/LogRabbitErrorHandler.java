f59bdadbf39ce812c02a1b11d3803b75835fa11b
LHPH <luislyly@hotmail.com>
2025-03-01 14:32:30 -0600

    Adjust in LogRabbitErrorHandler due upgrading Spring AMQP

--- src/main/java/com/kyc/notifications/handlers/LogRabbitErrorHandler.java
+++ src/main/java/com/kyc/notifications/handlers/LogRabbitErrorHandler.java
@@ -1,5 +1,6 @@
 package com.kyc.notifications.handlers;
 
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
@@ -13,7 +14,7 @@ public class LogRabbitErrorHandler implements RabbitListenerErrorHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(LogRabbitErrorHandler.class);
 
     @Override
-    public Object handleError(Message message, org.springframework.messaging.Message<?> message1, ListenerExecutionFailedException e) throws Exception {
+    public Object handleError(Message message, Channel channel, org.springframework.messaging.Message<?> message1, ListenerExecutionFailedException e) throws Exception {
 
         LOGGER.warn("In handle error {}",message.getMessageProperties());
         throw e;
